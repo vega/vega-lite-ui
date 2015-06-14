580fb26bc842b7441752b48ec129af5cb9177341
Kanit "Ham" Wongsuphasawat <kanitw@gmail.com>
2015-06-14 14:29:49 -0400

    fix watcher urls

--- gulp/watch.js
+++ gulp/watch.js
@@ -6,10 +6,9 @@ var paths = gulp.paths;
 
 gulp.task('watch', function () {
   gulp.watch([
-    paths.src + '/*.html',
-    paths.src + '/{app,components}/**/*.scss',
-    paths.src + '/{app,components}/**/*.js',
-    paths.src + '/assets/*.scss',
+    paths.src + '/**/*.html',
+    paths.src + '/**/*.scss',
+    paths.src + '/**/*.js',
     'bower.json'
   ], ['jshint', 'test', 'build']);
 });
