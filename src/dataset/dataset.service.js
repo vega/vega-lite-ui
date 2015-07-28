4d8c1c3f7117ab3d25c55fa0f91cf386d99038a4
Dominik Moritz <domoritz@gmail.com>
2015-07-27 22:03:52 -0700

    Add samples. Fixes #50

--- src/dataset/dataset.service.js
+++ src/dataset/dataset.service.js
@@ -125,100 +125,108 @@ angular.module('vlui')
     return field.name;
   };
 
-  Dataset.fieldOrderBy.cardinality = function(field, stats){
+  Dataset.fieldOrderBy.cardinality = function(field, stats) {
     return stats[field.name].distinct;
   };
 
-    Dataset.fieldOrder = Dataset.fieldOrderBy.typeThenName;
-    Dataset.getSchema = function(data, stats, order) {
-      var types = dl.type.inferAll(data),
-        schema = _.reduce(types, function(s, type, name) {
-          var field = {
-            name: name,
-            type: vl.data.types[type],
-            primitiveType: type
-          };
+  Dataset.fieldOrder = Dataset.fieldOrderBy.typeThenName;
 
-          if (field.type === 'Q' && stats[field.name].distinct <= 5) {
-            field.type = 'O';
-          }
+  Dataset.getSchema = function(data, stats, order) {
+    var types = dl.type.inferAll(data),
+      schema = _.reduce(types, function(s, type, name) {
+        var field = {
+          name: name,
+          type: vl.data.types[type],
+          primitiveType: type
+        };
 
-          s.push(field);
-          return s;
-        }, []);
+        if (field.type === 'Q' && stats[field.name].distinct <= 5) {
+          field.type = 'O';
+        }
 
-      schema = dl.stablesort(schema, order || Dataset.fieldOrderBy.typeThenName, Dataset.fieldOrderBy.name);
+        s.push(field);
+        return s;
+      }, []);
 
-      schema.push(vl.field.count());
-      return schema;
-    };
+    schema = dl.stablesort(schema, order || Dataset.fieldOrderBy.typeThenName, Dataset.fieldOrderBy.name);
 
-    // update the schema and stats
-    Dataset.onUpdate = [];
+    schema.push(vl.field.count());
+    return schema;
+  };
 
-    Dataset.update = function(dataset) {
-      var updatePromise;
+  // update the schema and stats
+  Dataset.onUpdate = [];
 
-      if (dataset.values) {
-        updatePromise = $q(function(resolve, reject) {
-          // jshint unused:false
-          Dataset.type = undefined;
-          Dataset.updateFromData(dataset, dataset.values);
-          resolve();
-        });
-      } else {
-        updatePromise = $http.get(dataset.url, {cache: true}).then(function(response) {
-          var data;
+  Dataset.update = function(dataset) {
+    var updatePromise;
 
-          // first see whether the data is JSON, otherwise try to parse CSV
-          if (_.isObject(response.data)) {
-             data = response.data;
-             Dataset.type = 'json';
-          } else {
-             var result = Papa.parse(response.data, {
-              dynamicTyping: true,
-              header: true
-            });
-
-            if (result.errors.length === 0) {
-              data = result.data;
-              Dataset.type = 'csv';
-            } else {
-              _.each(result.errors, function(err) {
-                Alerts.add(err.message, 2000);
-              });
-              return;
-            }
-          }
-
-          Dataset.updateFromData(dataset, data);
-        });
-      }
-
-      Dataset.onUpdate.forEach(function(listener) {
-        updatePromise = updatePromise.then(listener);
+    if (dataset.values) {
+      updatePromise = $q(function(resolve, reject) {
+        // jshint unused:false
+        Dataset.type = undefined;
+        Dataset.updateFromData(dataset, dataset.values);
+        resolve();
       });
+    } else {
+      updatePromise = $http.get(dataset.url, {cache: true}).then(function(response) {
+        var data;
 
-      return updatePromise;
-    };
+        // first see whether the data is JSON, otherwise try to parse CSV
+        if (_.isObject(response.data)) {
+           data = response.data;
+           Dataset.type = 'json';
+        } else {
+           var result = Papa.parse(response.data, {
+            dynamicTyping: true,
+            header: true
+          });
 
-    Dataset.updateFromData = function(dataset, data) {
-      Dataset.data = data;
+          if (result.errors.length === 0) {
+            data = result.data;
+            Dataset.type = 'csv';
+          } else {
+            _.each(result.errors, function(err) {
+              Alerts.add(err.message, 2000);
+            });
+            return;
+          }
+        }
 
-      Dataset.currentDataset = dataset;
-      Dataset.stats = vl.data.stats(Dataset.data);
-      Dataset.dataschema = Dataset.getSchema(Dataset.data, Dataset.stats);
-      Dataset.dataschema.byName = getNameMap(Dataset.dataschema);
-    };
+        Dataset.updateFromData(dataset, data);
+      });
+    }
 
-    Dataset.add = function(dataset) {
-      if (!dataset.id) {
-        dataset.id = dataset.url;
+    Dataset.onUpdate.forEach(function(listener) {
+      updatePromise = updatePromise.then(listener);
+    });
+
+    return updatePromise;
+  };
+
+  Dataset.updateFromData = function(dataset, data) {
+    Dataset.data = data;
+
+    Dataset.currentDataset = dataset;
+    Dataset.stats = vl.data.stats(Dataset.data);
+
+    for (var fieldName in Dataset.stats) {
+      if (fieldName !== '*') {
+        Dataset.stats[fieldName].sample = _.sample(_.pluck(Dataset.data, fieldName), 7);
       }
-      datasets.push(dataset);
+    }
 
-      return dataset;
-    };
+    Dataset.dataschema = Dataset.getSchema(Dataset.data, Dataset.stats);
+    Dataset.dataschema.byName = getNameMap(Dataset.dataschema);
+  };
 
-    return Dataset;
-  });
+  Dataset.add = function(dataset) {
+    if (!dataset.id) {
+      dataset.id = dataset.url;
+    }
+    datasets.push(dataset);
+
+    return dataset;
+  };
+
+  return Dataset;
+});
