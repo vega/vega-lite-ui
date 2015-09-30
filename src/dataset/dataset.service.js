89c799accad7a724ed909c9aa03c25f44cb3eb06
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-30 16:35:24 -0400

    Move sample datasets into a constant in the vlui module

--- src/dataset/dataset.service.js
+++ src/dataset/dataset.service.js
@@ -1,72 +1,5 @@
 'use strict';
 
-var datasets = [{
-  name: 'Barley',
-  url: 'data/barley.json',
-  id: 'barley',
-  group: 'sample'
-},{
-  name: 'Cars',
-  url: 'data/cars.json',
-  id: 'cars',
-  group: 'sample'
-},{
-  name: 'Crimea',
-  url: 'data/crimea.json',
-  id: 'crimea',
-  group: 'sample'
-},{
-  name: 'Driving',
-  url: 'data/driving.json',
-  id: 'driving',
-  group: 'sample'
-},{
-  name: 'Iris',
-  url: 'data/iris.json',
-  id: 'iris',
-  group: 'sample'
-},{
-  name: 'Jobs',
-  url: 'data/jobs.json',
-  id: 'jobs',
-  group: 'sample'
-},{
-  name: 'Population',
-  url: 'data/population.json',
-  id: 'population',
-  group: 'sample'
-},{
-  name: 'Movies',
-  url: 'data/movies.json',
-  id: 'movies',
-  group: 'sample'
-},{
-  name: 'Birdstrikes',
-  url: 'data/birdstrikes.json',
-  id: 'birdstrikes',
-  group: 'sample'
-},{
-  name: 'Burtin',
-  url: 'data/burtin.json',
-  id: 'burtin',
-  group: 'sample'
-},{
-  name: 'Budget 2016',
-  url: 'data/budget.json',
-  id: 'budget',
-  group: 'sample'
-},{
-  name: 'Climate Normals',
-  url: 'data/climate.json',
-  id: 'climate',
-  group: 'sample'
-},{
-  name: 'Campaigns',
-  url: 'data/weball26.json',
-  id: 'weball26',
-  group: 'sample'
-}];
-
 function getNameMap(dataschema) {
   return dataschema.reduce(function(m, field) {
     m[field.name] = field;
@@ -75,8 +8,11 @@ function getNameMap(dataschema) {
 }
 
 angular.module('vlui')
-  .factory('Dataset', function($http, $q, Alerts, _, dl, vl) {
+  .factory('Dataset', function($http, $q, Alerts, _, dl, vl, SampleData) {
     var Dataset = {};
+
+    // Start with the list of sample datasets
+    var datasets = SampleData;
 
     Dataset.datasets = datasets;
     Dataset.dataset = datasets[1];
