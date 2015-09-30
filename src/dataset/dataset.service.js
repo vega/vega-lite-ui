b68f373d3949b174e1abedd132345ae73801df3d
K. Adam White <kadamwhite@users.noreply.github.com>
2015-09-30 16:32:53 -0400

    Whitespace: normalize indentation of Dataset factory

--- src/dataset/dataset.service.js
+++ src/dataset/dataset.service.js
@@ -86,125 +86,125 @@ angular.module('vlui')
     Dataset.stats = {};
     Dataset.type = undefined;
 
-  var typeOrder = {
-    N: 0,
-    O: 0,
-    G: 2,
-    T: 3,
-    Q: 4
-  };
+    var typeOrder = {
+      N: 0,
+      O: 0,
+      G: 2,
+      T: 3,
+      Q: 4
+    };
 
-  Dataset.fieldOrderBy = {};
+    Dataset.fieldOrderBy = {};
 
-  Dataset.fieldOrderBy.type = function(field) {
-    if (field.aggregate==='count') return 4;
-    return typeOrder[field.type];
-  };
+    Dataset.fieldOrderBy.type = function(field) {
+      if (field.aggregate==='count') return 4;
+      return typeOrder[field.type];
+    };
 
-  Dataset.fieldOrderBy.typeThenName = function(field) {
-    return Dataset.fieldOrderBy.type(field) + '_' +
-      (field.aggregate === 'count' ? '~' : field.name.toLowerCase());
-      // ~ is the last character in ASCII
-  };
+    Dataset.fieldOrderBy.typeThenName = function(field) {
+      return Dataset.fieldOrderBy.type(field) + '_' +
+        (field.aggregate === 'count' ? '~' : field.name.toLowerCase());
+        // ~ is the last character in ASCII
+    };
 
-  Dataset.fieldOrderBy.original = function() {
-    return 0; // no swap will occur
-  };
+    Dataset.fieldOrderBy.original = function() {
+      return 0; // no swap will occur
+    };
 
-  Dataset.fieldOrderBy.name = function(field) {
-    return field.name;
-  };
+    Dataset.fieldOrderBy.name = function(field) {
+      return field.name;
+    };
 
-  Dataset.fieldOrderBy.cardinality = function(field, stats) {
-    return stats[field.name].distinct;
-  };
+    Dataset.fieldOrderBy.cardinality = function(field, stats) {
+      return stats[field.name].distinct;
+    };
 
-  Dataset.fieldOrder = Dataset.fieldOrderBy.typeThenName;
+    Dataset.fieldOrder = Dataset.fieldOrderBy.typeThenName;
 
-  Dataset.getSchema = function(data, stats, order) {
-    var types = dl.type.inferAll(data),
-      schema = _.reduce(types, function(s, type, name) {
-        var field = {
-          name: name,
-          type: vl.data.types[type],
-          primitiveType: type
-        };
+    Dataset.getSchema = function(data, stats, order) {
+      var types = dl.type.inferAll(data),
+        schema = _.reduce(types, function(s, type, name) {
+          var field = {
+            name: name,
+            type: vl.data.types[type],
+            primitiveType: type
+          };
 
-        if (field.type === 'Q' && stats[field.name].distinct <= 5) {
-          field.type = 'O';
-        }
+          if (field.type === 'Q' && stats[field.name].distinct <= 5) {
+            field.type = 'O';
+          }
 
-        s.push(field);
-        return s;
-      }, []);
+          s.push(field);
+          return s;
+        }, []);
 
-    schema = dl.stablesort(schema, order || Dataset.fieldOrderBy.typeThenName, Dataset.fieldOrderBy.name);
+      schema = dl.stablesort(schema, order || Dataset.fieldOrderBy.typeThenName, Dataset.fieldOrderBy.name);
 
-    schema.push(vl.field.count());
-    return schema;
-  };
+      schema.push(vl.field.count());
+      return schema;
+    };
 
-  // update the schema and stats
-  Dataset.onUpdate = [];
+    // update the schema and stats
+    Dataset.onUpdate = [];
 
-  Dataset.update = function(dataset) {
-    var updatePromise;
+    Dataset.update = function(dataset) {
+      var updatePromise;
 
-    if (dataset.values) {
-      updatePromise = $q(function(resolve, reject) {
-        // jshint unused:false
-        Dataset.type = undefined;
-        Dataset.updateFromData(dataset, dataset.values);
-        resolve();
-      });
-    } else {
-      updatePromise = $http.get(dataset.url, {cache: true}).then(function(response) {
-        var data;
+      if (dataset.values) {
+        updatePromise = $q(function(resolve, reject) {
+          // jshint unused:false
+          Dataset.type = undefined;
+          Dataset.updateFromData(dataset, dataset.values);
+          resolve();
+        });
+      } else {
+        updatePromise = $http.get(dataset.url, {cache: true}).then(function(response) {
+          var data;
 
-        // first see whether the data is JSON, otherwise try to parse CSV
-        if (_.isObject(response.data)) {
-           data = response.data;
-           Dataset.type = 'json';
-        } else {
-          data = dl.read(response.data, {type: 'csv'});
-          Dataset.type = 'csv';
-        }
+          // first see whether the data is JSON, otherwise try to parse CSV
+          if (_.isObject(response.data)) {
+             data = response.data;
+             Dataset.type = 'json';
+          } else {
+            data = dl.read(response.data, {type: 'csv'});
+            Dataset.type = 'csv';
+          }
 
-        Dataset.updateFromData(dataset, data);
-      });
-    }
-
-    Dataset.onUpdate.forEach(function(listener) {
-      updatePromise = updatePromise.then(listener);
-    });
-
-    return updatePromise;
-  };
-
-  Dataset.updateFromData = function(dataset, data) {
-    Dataset.data = data;
-
-    Dataset.currentDataset = dataset;
-    Dataset.stats = vl.data.stats(Dataset.data);
-
-    for (var fieldName in Dataset.stats) {
-      if (fieldName !== '*') {
-        Dataset.stats[fieldName].sample = _.sample(_.pluck(Dataset.data, fieldName), 7);
+          Dataset.updateFromData(dataset, data);
+        });
       }
-    }
 
-    Dataset.dataschema = Dataset.getSchema(Dataset.data, Dataset.stats);
-    Dataset.dataschema.byName = getNameMap(Dataset.dataschema);
-  };
+      Dataset.onUpdate.forEach(function(listener) {
+        updatePromise = updatePromise.then(listener);
+      });
 
-  Dataset.add = function(dataset) {
-    if (!dataset.id) {
-      dataset.id = dataset.url;
-    }
-    datasets.push(dataset);
+      return updatePromise;
+    };
 
-    return dataset;
-  };
+    Dataset.updateFromData = function(dataset, data) {
+      Dataset.data = data;
 
-  return Dataset;
-});
+      Dataset.currentDataset = dataset;
+      Dataset.stats = vl.data.stats(Dataset.data);
+
+      for (var fieldName in Dataset.stats) {
+        if (fieldName !== '*') {
+          Dataset.stats[fieldName].sample = _.sample(_.pluck(Dataset.data, fieldName), 7);
+        }
+      }
+
+      Dataset.dataschema = Dataset.getSchema(Dataset.data, Dataset.stats);
+      Dataset.dataschema.byName = getNameMap(Dataset.dataschema);
+    };
+
+    Dataset.add = function(dataset) {
+      if (!dataset.id) {
+        dataset.id = dataset.url;
+      }
+      datasets.push(dataset);
+
+      return dataset;
+    };
+
+    return Dataset;
+  });
