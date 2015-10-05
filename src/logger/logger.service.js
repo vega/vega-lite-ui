a28ec5f855b806b27325b268c2a406e70d0b4ca7
Jim Vallandingham <vlandham@gmail.com>
2015-10-05 09:05:59 -0400

    convert cat to category. remove unused function

--- src/logger/logger.service.js
+++ src/logger/logger.service.js
@@ -24,47 +24,47 @@ angular.module('vlui')
 
     service.actions = {
       // DATA
-      INITIALIZE: {cat: 'DATA', id: 'INITIALIZE', level: service.levels.DEBUG},
-      UNDO: {cat: 'DATA', id: 'UNDO', level: service.levels.INFO},
-      REDO: {cat: 'DATA', id: 'REDO', level: service.levels.INFO},
-      DATASET_CHANGE: {cat: 'DATA', id: 'DATASET_CHANGE', level: service.levels.INFO},
-      DATASET_OPEN: {cat: 'DATA', id: 'DATASET_OPEN', level: service.levels.INFO},
-      DATASET_NEW_PASTE: {cat: 'DATA', id: 'DATASET_NEW_PASTE', level: service.levels.INFO},
-      DATASET_NEW_URL: {cat: 'DATA', id: 'DATASET_NEW_URL', level: service.levels.INFO},
+      INITIALIZE: {category: 'DATA', id: 'INITIALIZE', level: service.levels.DEBUG},
+      UNDO: {category: 'DATA', id: 'UNDO', level: service.levels.INFO},
+      REDO: {category: 'DATA', id: 'REDO', level: service.levels.INFO},
+      DATASET_CHANGE: {category: 'DATA', id: 'DATASET_CHANGE', level: service.levels.INFO},
+      DATASET_OPEN: {category: 'DATA', id: 'DATASET_OPEN', level: service.levels.INFO},
+      DATASET_NEW_PASTE: {category: 'DATA', id: 'DATASET_NEW_PASTE', level: service.levels.INFO},
+      DATASET_NEW_URL: {category: 'DATA', id: 'DATASET_NEW_URL', level: service.levels.INFO},
       // BOOKMARK
-      BOOKMARK_ADD: {cat: 'BOOKMARK', id:'BOOKMARK_ADD', level: service.levels.INFO},
-      BOOKMARK_REMOVE: {cat: 'BOOKMARK', id:'BOOKMARK_REMOVE', level: service.levels.INFO},
-      BOOKMARK_OPEN: {cat: 'BOOKMARK', id:'BOOKMARK_OPEN', level: service.levels.INFO},
-      BOOKMARK_CLOSE: {cat: 'BOOKMARK', id:'BOOKMARK_CLOSE', level: service.levels.INFO},
-      BOOKMARK_CLEAR: {cat: 'BOOKMARK', id: 'BOOKMARK_CLEAR', level: service.levels.INFO},
+      BOOKMARK_ADD: {category: 'BOOKMARK', id:'BOOKMARK_ADD', level: service.levels.INFO},
+      BOOKMARK_REMOVE: {category: 'BOOKMARK', id:'BOOKMARK_REMOVE', level: service.levels.INFO},
+      BOOKMARK_OPEN: {category: 'BOOKMARK', id:'BOOKMARK_OPEN', level: service.levels.INFO},
+      BOOKMARK_CLOSE: {category: 'BOOKMARK', id:'BOOKMARK_CLOSE', level: service.levels.INFO},
+      BOOKMARK_CLEAR: {category: 'BOOKMARK', id: 'BOOKMARK_CLEAR', level: service.levels.INFO},
       // CHART
-      CHART_MOUSEOVER: {cat: 'CHART', id:'CHART_MOUSEOVER', level: service.levels.DEBUG},
-      CHART_MOUSEOUT: {cat: 'CHART', id:'CHART_MOUSEOUT', level: service.levels.DEBUG},
-      CHART_RENDER: {cat: 'CHART', id:'CHART_RENDER', level: service.levels.DEBUG},
-      CHART_EXPOSE: {cat: 'CHART', id:'CHART_EXPOSE', level: service.levels.DEBUG},
-      CHART_TOOLTIP: {cat: 'CHART', id:'CHART_TOOLTIP', level: service.levels.DEBUG},
-      CHART_TOOLTIP_END: {cat: 'CHART', id:'CHART_TOOLTIP_END', level: service.levels.DEBUG},
+      CHART_MOUSEOVER: {category: 'CHART', id:'CHART_MOUSEOVER', level: service.levels.DEBUG},
+      CHART_MOUSEOUT: {category: 'CHART', id:'CHART_MOUSEOUT', level: service.levels.DEBUG},
+      CHART_RENDER: {category: 'CHART', id:'CHART_RENDER', level: service.levels.DEBUG},
+      CHART_EXPOSE: {category: 'CHART', id:'CHART_EXPOSE', level: service.levels.DEBUG},
+      CHART_TOOLTIP: {category: 'CHART', id:'CHART_TOOLTIP', level: service.levels.DEBUG},
+      CHART_TOOLTIP_END: {category: 'CHART', id:'CHART_TOOLTIP_END', level: service.levels.DEBUG},
 
-      SORT_TOGGLE: {cat: 'CHART', id:'SORT_TOGGLE', level: service.levels.INFO},
-      MARKTYPE_TOGGLE: {cat: 'CHART', id:'MARKTYPE_TOGGLE', level: service.levels.INFO},
-      DRILL_DOWN_OPEN: {cat: 'CHART', id:'DRILL_DOWN_OPEN', level: service.levels.INFO},
-      DRILL_DOWN_CLOSE: {cat: 'CHART', id: 'DRILL_DOWN_CLOSE', level: service.levels.INFO},
-      LOG_TOGGLE: {cat: 'CHART', id: 'LOG_TOGGLE', level: service.levels.INFO},
-      TRANSPOSE_TOGGLE: {cat: 'CHART', id: 'TRANSPOSE_TOGGLE', level: service.levels.INFO},
-      NULL_FILTER_TOGGLE: {cat: 'CHART', id:'NULL_FILTER_TOGGLE', level: service.levels.INFO},
+      SORT_TOGGLE: {category: 'CHART', id:'SORT_TOGGLE', level: service.levels.INFO},
+      MARKTYPE_TOGGLE: {category: 'CHART', id:'MARKTYPE_TOGGLE', level: service.levels.INFO},
+      DRILL_DOWN_OPEN: {category: 'CHART', id:'DRILL_DOWN_OPEN', level: service.levels.INFO},
+      DRILL_DOWN_CLOSE: {category: 'CHART', id: 'DRILL_DOWN_CLOSE', level: service.levels.INFO},
+      LOG_TOGGLE: {category: 'CHART', id: 'LOG_TOGGLE', level: service.levels.INFO},
+      TRANSPOSE_TOGGLE: {category: 'CHART', id: 'TRANSPOSE_TOGGLE', level: service.levels.INFO},
+      NULL_FILTER_TOGGLE: {category: 'CHART', id:'NULL_FILTER_TOGGLE', level: service.levels.INFO},
 
-      CLUSTER_SELECT: {cat: 'CHART', id:'CLUSTER_SELECT', level: service.levels.INFO},
-      LOAD_MORE: {cat: 'CHART', id:'LOAD_MORE', level: service.levels.INFO},
+      CLUSTER_SELECT: {category: 'CHART', id:'CLUSTER_SELECT', level: service.levels.INFO},
+      LOAD_MORE: {category: 'CHART', id:'LOAD_MORE', level: service.levels.INFO},
 
       // FIELDS
-      FIELDS_CHANGE: {cat: 'FIELDS', id: 'FIELDS_CHANGE', level: service.levels.INFO},
-      FIELDS_RESET: {cat: 'FIELDS', id: 'FIELDS_RESET', level: service.levels.INFO},
-      FUNC_CHANGE: {cat: 'FIELDS', id: 'FUNC_CHANGE', level: service.levels.INFO},
+      FIELDS_CHANGE: {category: 'FIELDS', id: 'FIELDS_CHANGE', level: service.levels.INFO},
+      FIELDS_RESET: {category: 'FIELDS', id: 'FIELDS_RESET', level: service.levels.INFO},
+      FUNC_CHANGE: {category: 'FIELDS', id: 'FUNC_CHANGE', level: service.levels.INFO},
 
       //POLESTAR
-      SPEC_CHANGE: {cat:'POLESTAR', id: 'SPEC_CHANGE', level: service.levels.DEBUG},
-      FIELD_DROP: {cat: 'POLESTAR', id: 'FIELD_DROP', level: service.levels.DEBUG},
-      MARK_CHANGE: {cat: 'POLESTAR', id: 'MARK_CHANGE', level: service.levels.DEBUG}
+      SPEC_CHANGE: {category:'POLESTAR', id: 'SPEC_CHANGE', level: service.levels.DEBUG},
+      FIELD_DROP: {category: 'POLESTAR', id: 'FIELD_DROP', level: service.levels.DEBUG},
+      MARK_CHANGE: {category: 'POLESTAR', id: 'MARK_CHANGE', level: service.levels.DEBUG}
     };
 
     service.logInteraction = function(action, label, data) {
@@ -73,17 +73,9 @@ angular.module('vlui')
       }
       var value = data ? data.value : undefined;
       if(action.level.rank >= service.levels.INFO.rank) {
-        Analytics.trackEvent(action.cat, action.id, label, value);
+        Analytics.trackEvent(action.category, action.id, label, value);
         console.log('[Logging] ', action.id, label, data);
       }
-    };
-
-    service.oldlogInteraction = function(action, data, diff) {
-      if (!consts.logging) {
-        return;
-      }
-
-      console.log('[Logging] ', action, data, diff);
     };
 
     service.logInteraction(service.actions.INITIALIZE, consts.appId);
