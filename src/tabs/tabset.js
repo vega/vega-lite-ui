78c1822efdbd91ffbd5ec55974a6475991450c81
K. Adam White <kadamwhite@users.noreply.github.com>
2015-10-06 18:37:14 -0400

    Organize the dataset add modal with a tab directive
    ![modal](https://cloud.githubusercontent.com/assets/442115/10324659/554d1e42-6c59-11e5-8cb3-9da6ee64f4f1.gif)
    
    This implements the tab directive approach @vlandham originally proposed
    to organize and simplify the layout of the modal directive. I spent a
    bit of time experimenting with the tabs on the side, as we had it in
    the original wireframe, and decided the tabs on the top were more idiomatic
    and readily accessible.

---
+++ src/tabs/tabset.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name vlui.directive:tabset
+ * @description
+ * # tabset
+ */
+angular.module('vlui')
+  .directive('tabset', function() {
+    return {
+      templateUrl: 'tabs/tabset.html',
+      restrict: 'E',
+      transclude: true,
+
+      // Interface for tabs to register themselves
+      controller: function() {
+        var self = this;
+
+        this.tabs = [];
+
+        this.addTab = function(tabScope) {
+          // First tab is always auto-activated; others auto-deactivated
+          tabScope.active = self.tabs.length === 0;
+          self.tabs.push(tabScope);
+        };
+
+        this.showTab = function(selectedTab) {
+          self.tabs.forEach(function(tab) {
+            // Activate the selected tab, deactivate all others
+            tab.active = tab === selectedTab;
+          });
+        };
+      },
+
+      // Expose controller to templates as "tabset"
+      controllerAs: 'tabset'
+    };
+  });
