a8f18764bf4a59ee7ea1e07d222f201a0c2ca2e6
Jim Vallandingham <vlandham@gmail.com>
2015-10-07 16:20:03 -0400

    Add ‘add your data’ link to Dataset Selector

--- src/dataset/datasetselector.js
+++ src/dataset/datasetselector.js
@@ -10,13 +10,17 @@ angular.module('vlui')
       link: function postLink(scope/*, element, attrs*/) {
         scope.Dataset = Dataset;
 
+        scope.loadData = function() {
+          // reset if no dataset has been set
+          Dataset.dataset = Dataset.currentDataset;
+          Logger.logInteraction(Logger.actions.DATASET_OPEN);
+          Modals.open('dataset-modal');
+          return;
+        };
+        
         scope.datasetChanged = function() {
           if (!Dataset.dataset) {
-            // reset if no dataset has been set
-            Dataset.dataset = Dataset.currentDataset;
-            Logger.logInteraction(Logger.actions.DATASET_OPEN);
-            Modals.open('dataset-modal');
-            return;
+            scope.loadData();
           }
 
           Dataset.update(Dataset.dataset);
