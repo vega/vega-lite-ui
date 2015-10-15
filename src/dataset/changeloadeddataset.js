c013ee6671714ec74266d9871be63b0c4f77ca23
K. Adam White <kadamwhite@users.noreply.github.com>
2015-10-15 17:53:41 -0400

    Move list of loaded datasets to a dataset modal tab
    See #113: this begins the work of moving the dataset list to a tab.
    
    ![image](https://cloud.githubusercontent.com/assets/442115/10528705/ccc60b24-7365-11e5-8d65-6d1818282481.png)

---
+++ src/dataset/changeloadeddataset.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name vega-lite-ui.filter:inGroup
+ * @function
+ * @description
+ * # inGroup
+ * Get datasets in a particular group
+ * @param  {String} datasetGroup One of "sample," "user", or "myria"
+ * @return {Array} An array of datasets in the specified group
+ */
+angular.module('vlui')
+  .filter('inGroup', function(_) {
+    return function(arr, datasetGroup) {
+      return _.where(arr, {
+        group: datasetGroup
+      });
+    };
+  });
+
+/**
+ * @ngdoc directive
+ * @name vlui.directive:changeLoadedDataset
+ * @description
+ * # changeLoadedDataset
+ */
+angular.module('vlui')
+  .directive('changeLoadedDataset', function (Dataset, _) {
+    return {
+      templateUrl: 'dataset/changeloadeddataset.html',
+      restrict: 'E',
+      require: '?^^modal',
+      replace: true,
+      scope: true,
+      link: function postLink(scope, element, attrs, modalController) {
+        // If this directive occurs within a a modal, give ourselves a way to close
+        // that modal once the add button has been clicked
+        function closeModal() {
+          if (modalController) {
+            modalController.close();
+          }
+        }
+
+        // Expose dataset object itself so current dataset can be marked
+        scope.Dataset = Dataset;
+
+        scope.userData = _.filter(Dataset.datasets, function(dataset) {
+          return dataset.group !== 'sample';
+        });
+
+        scope.sampleData = _.where(Dataset.datasets, {
+          group: 'sample'
+        });
+
+        scope.$watch(function() {
+          return Dataset.datasets.length;
+        }, function() {
+          scope.userData = _.filter(Dataset.datasets, function(dataset) {
+            return dataset.group !== 'sample';
+          });
+        });
+
+        scope.selectDataset = function(dataset) {
+          // Activate the selected dataset
+          Dataset.update(dataset);
+          closeModal();
+        };
+      }
+    };
+  });
--- src/dataset/datasetselector.js
+++ src/dataset/datasetselector.js
@@ -1,29 +1,16 @@
 'use strict';
 
 angular.module('vlui')
-  .directive('datasetSelector', function(Dataset, Modals, Logger) {
+  .directive('datasetSelector', function(Modals, Logger) {
     return {
       templateUrl: 'dataset/datasetselector.html',
       restrict: 'E',
       replace: true,
       scope: {},
       link: function postLink(scope/*, element, attrs*/) {
-        scope.Dataset = Dataset;
-
-        scope.loadData = function() {
-          // reset if no dataset has been set
-          Dataset.dataset = Dataset.currentDataset;
+        scope.loadDataset = function() {
           Logger.logInteraction(Logger.actions.DATASET_OPEN);
           Modals.open('dataset-modal');
-          return;
-        };
-        
-        scope.datasetChanged = function() {
-          if (!Dataset.dataset) {
-            scope.loadData();
-          }
-
-          Dataset.update(Dataset.dataset);
         };
       }
     };
