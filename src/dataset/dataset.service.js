026a7db0a7d756387f2533c6fb82115c9723e6bf
Dominik Moritz <domoritz@gmail.com>
2015-11-14 16:56:50 -0800

    Convert to string types from vl types

--- src/dataset/dataset.service.js
+++ src/dataset/dataset.service.js
@@ -62,7 +62,7 @@ angular.module('vlui')
         schema = _.reduce(types, function(s, type, name) {
           var field = {
             name: name,
-            type: vl.data.types[type],
+            type: vl.Type[vl.data.TYPES[type]],  // from dl type to vl type to string representation
             primitiveType: type
           };
 
