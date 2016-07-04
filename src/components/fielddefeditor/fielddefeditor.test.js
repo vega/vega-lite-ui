74d9a738ef68d2868f4272fc7733ffc38ab8354b
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-07-04 15:39:34 -0700

    Migrate `FieldDefEditor`, `FunctionSelect`, `PropertyEditor`, and `Shelves` to `vlui`

---
+++ src/components/fielddefeditor/fielddefeditor.test.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* global vl:true, vlSchema:true */
+
+describe('Directive: fieldDefEditor', function() {
+
+  // load the directive's module
+  beforeEach(module('vlui', function($provide) {
+    $provide.constant('vl', vl);
+    $provide.constant('vlSchema', vlSchema);
+    $provide.constant('Drop', function() {});
+  }));
+
+  var element, scope, $compile;
+
+  beforeEach(module('vlui', function($provide) {
+    $provide.constant('Dataset', {
+      stats: {
+        a: {}
+      },
+      onUpdate: []
+    });
+  }));
+
+  beforeEach(inject(function($rootScope, _$compile_) {
+    scope = $rootScope.$new();
+    scope.channel = 'x';
+    scope.encoding = {'x': {}};
+
+    $compile = _$compile_;
+  }));
+
+  it('should show title', function() {
+    element = angular.element('<field-def-editor channel="channel" encoding="encoding" schema="{properties:{}}"></field-def-editor>');
+    element = $compile(element)(scope);
+    scope.$digest();
+    expect(element.find('.shelf-label').text().trim()).to.eql('x');
+  });
+
+  describe('fieldDrop', function() {
+    it('should initially have placeholder', function() {
+      element = angular.element('<field-def-editor channel="channel" encoding="encoding" schema="schema"></field-def-editor>');
+      element = $compile(element)(scope);
+      scope.$digest();
+      expect(element.find('.placeholder').length).to.eql(1);
+    });
+
+    it('should show correct field name when dropped', function() {
+      // jshint unused:false
+      //TODO
+    });
+  });
+
+  describe('shelfProperties', function() {
+    it('should change properties correctly', function() {
+      // jshint unused:false
+      //TODO
+    });
+  });
+
+  describe('shelfFunctions', function() {
+    it('should change function correctly', function() {
+      // jshint unused:false
+      //TODO
+    });
+  });
+});
---
+++ src/components/shelves/shelves.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('vlui')
+  .directive('shelves', function() {
+
+    return {
+      templateUrl: 'components/shelves/shelves.html',
+      restrict: 'E',
+      scope: {
+        spec: '='
+      },
+      replace: true,
+      controller: function($scope, vl, Config, Dataset, Logger, Pills) {
+        $scope.markChange = function() {
+          Logger.logInteraction(Logger.actions.MARK_CHANGE, $scope.spec.mark);
+        };
+
+        $scope.transpose = function(){
+          vl.spec.transpose($scope.spec);
+        };
+
+        $scope.clear = function(){
+          Pills.reset();
+        };
+
+        $scope.$watch('spec', function(spec) {
+          Logger.logInteraction(Logger.actions.SPEC_CHANGE, spec);
+
+          Pills.update(spec);
+        }, true); //, true /* watch equality rather than reference */);
+      }
+    };
+  });
