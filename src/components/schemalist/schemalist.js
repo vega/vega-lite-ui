19ceb06c1918192dc1fbb32e938fe5c486305e25
Kanit Wongsuphasawat <kanitw@gmail.com>
2016-07-06 10:01:00 -0700

    make schema list no longer depends on Dataset internally

--- src/components/schemalist/schemalist.js
+++ src/components/schemalist/schemalist.js
@@ -1,16 +1,14 @@
 'use strict';
 
 angular.module('vlui')
-  .directive('schemaList', function(Dataset) {
+  .directive('schemaList', function() {
     return {
       templateUrl: 'components/schemalist/schemalist.html',
       restrict: 'E',
-      scope: {},
-      replace: true,
-      // link: function postLink(scope, element /*, attrs*/) {
-      // },
-      controller: function($scope) {
-        $scope.Dataset = Dataset;
-      }
+      scope: {
+        orderBy: '=',
+        fieldDefs: '='
+      },
+      replace: true
     };
   });
