vega/vega-lite-ui

commit 2c67d837d6f2f6fc9dbb6c2fb260a140172dcda8

Correctly display tooltip for data type
Fix https://github.com/uwdata/voyager2/issues/194

src/components/fieldinfo/fieldinfo.js
@@ -7,7 +7,7 @@
  * # fieldInfo
  */
 angular.module('vlui')
-  .directive('fieldInfo', function (ANY, Drop, vl, cql) {
+  .directive('fieldInfo', function (ANY, Drop, vl, cql, Dataset) {
     return {
       templateUrl: 'components/fieldinfo/fieldinfo.html',
       restrict: 'E',
@@ -129,22 +129,29 @@ angular.module('vlui')
             var val = null;
             for (var i = 0; i < type.enum.length; i++) {
               var _type = type.enum[i];
+              var v = dict ? dict[_type] : _type;
               if (val === null) {
-                val = dict[_type];
+                val = v;
               } else {
-                if (val !== dict[_type]) {
+                if (val !== v) {
                   return ANY; // If there are many conflicting types
                 }
               }
             }
             return val;
           }
-          return dict[type];
+          return dict ? dict[type] : type;
         }
 
         var fieldDefWatcher = scope.$watch('fieldDef.type', function(type) {
           scope.icon = getTypeDictValue(type, TYPE_ICONS);
-          scope.typeName = getTypeDictValue(type, TYPE_NAMES);
+          var typeName = type;
+          if (typeName === 'ordinal' || typeName === 'nominal') {
+            typeName += (' (' + Dataset.schema.primitiveType(scope.fieldDef.field) + ')');
+          } else if (type.enum) {
+            typeName = type.enum[0]; // FIXME join them if we support many types
+          }
+          scope.typeName = typeName;
         });
 
         scope.$on('$destroy', function() {

commit d0563e3e2881f8f97df859e7a8564299b6743393

[Follow-up] Fix undefined error
(introduced in 2c67d837d6f2f6fc9dbb6c2fb260a140172dcda8)

src/components/fieldinfo/fieldinfo.js
@@ -148,7 +148,7 @@ angular.module('vlui')
           var typeName = type;
           if (typeName === 'ordinal' || typeName === 'nominal') {
             typeName += (' (' + Dataset.schema.primitiveType(scope.fieldDef.field) + ')');
-          } else if (type.enum) {
+          } else if (type && type.enum) {
             typeName = type.enum[0]; // FIXME join them if we support many types
           }
           scope.typeName = typeName;